26a46721e09995f58d1dc843e72768aa0369b8ec
Lenovo <Lenovo@LAPTOP-91EJ1UJ3>
2022-10-02 15:06:48 +0530

    Third Commit

---
+++ src/Basics/Test3.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class Test3 
+{
+	 public static void main(String[] args) 
+	  {
+		  System.out.println("My GF name is Preara Patil");
+		  m1();
+		  m2();
+		  m3();
+	  }	
+	  public static void m1() 
+	  {
+		  System.out.println("Nishan Patil");
+	  }
+	  public static void m2() 
+	  {
+		  System.out.println("Mrunal Patil");
+	  }
+	  public static void m3() 
+	  {
+		  System.out.println("Harshada Patil");
+	  }
+}
